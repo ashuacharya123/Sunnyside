2584500799038f2e59a7f7a11c1c20fc2af66736
ashuacharya <ashuacharya622@gmail.com>
2021-11-10 11:49:14 +0545

    complted for my phone

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -7,10 +7,12 @@ const Clients = () => {
   return (
     <section className="clients">
       <h3 className="clients__heading">Client testimonials</h3>
-
       <div className="clients__card">
         <img src={pp1} alt="dp" />
-        <p className="clients__card__text"></p>
+        <p className="clients__card__text">
+          We put our trust in Sunnyside and they delivered, making sure our
+          needs were met and deadlines were always hit.
+        </p>
         <h2 className="clients__card__name">
           Emily R. <span>Marketing Director</span>
         </h2>
@@ -18,17 +20,22 @@ const Clients = () => {
 
       <div className="clients__card">
         <img src={pp2} alt="dp" />
-        <p className="clients__card__text"></p>
+        <p className="clients__card__text">
+          Sunnyside's enthusiasm coupled with their keen interest in our brand's
+          success made it a satisfying and enjoyable experience
+        </p>
         <h2 className="clients__card__name">
-          Emily R. <span>Marketing Director</span>
+          Thomas S. <span>Chief Operating Officer</span>
         </h2>
       </div>
-
       <div className="clients__card">
         <img src={pp3} alt="dp" />
-        <p className="clients__card__text"></p>
+        <p className="clients__card__text">
+          Increadible end result! Our sales increased over 400% when we worked
+          with Sunnyside. Highly recommended!
+        </p>
         <h2 className="clients__card__name">
-          Emily R. <span>Marketing Director</span>
+          Jennie F. <span>Business Owner</span>
         </h2>
       </div>
     </section>
