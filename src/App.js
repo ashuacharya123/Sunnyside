f55eb777694f53269c60cf962e0b0db9581a8324
ashuacharya <ashuacharya622@gmail.com>
2022-08-05 09:54:57 +0545

    updated name

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, {useState} from "react";
 
 import Header from "./components/Header";
 import Grid from "./components/Grid";
@@ -10,41 +10,41 @@ function App() {
   const [state, setstate] = useState("d");
 
   return (
-    <>
-      <input type="checkbox" id="check" />
-      <div className="container">
-        <div className="menu__container">
-          <ul className="menu__content">
-            <li id={state === "a" ? "active" : ""}>
-              <a onClick={(e) => setstate("a")} href="#">
-                About
-              </a>
-            </li>
-            <li id={state === "b" ? "active" : ""}>
-              <a onClick={(e) => setstate("b")} href="#">
-                Services
-              </a>
-            </li>
-            <li id={state === "c" ? "active" : ""}>
-              <a onClick={(e) => setstate("c")} href="#">
-                Projects
-              </a>
-            </li>
-            <li id={state === "d" ? "active" : ""}>
-              <a onClick={(e) => setstate("d")} href="#">
-                Contact
-              </a>
-            </li>
-          </ul>
+      <>
+        <input type="checkbox" id="check"/>
+        <div className="container">
+          <div className="menu__container">
+            <ul className="menu__content">
+              <li id={state === "a" ? "active" : ""}>
+                <a onClick={(e) => setstate("a")} href="#">
+                  About
+                </a>
+              </li>
+              <li id={state === "b" ? "active" : ""}>
+                <a onClick={(e) => setstate("b")} href="#">
+                  Services
+                </a>
+              </li>
+              <li id={state === "c" ? "active" : ""}>
+                <a onClick={(e) => setstate("c")} href="#">
+                  Projects
+                </a>
+              </li>
+              <li id={state === "d" ? "active" : ""}>
+                <a onClick={(e) => setstate("d")} href="#">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="content">
+            <Header/>
+            <Grid/>
+            <Clients/>
+            <Footer/>
+          </div>
         </div>
-        <div className="content">
-          <Header />
-          <Grid />
-          <Clients />
-          <Footer />
-        </div>
-      </div>
-    </>
+      </>
   );
 }
 
